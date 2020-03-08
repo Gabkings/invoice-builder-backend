27adf9210c174d5191e7455ce9a8898b2bf21757
gab <gab@localhost.localdomain>
2020-03-08 23:19:52 +0300

    documented api using swagger documentation open source

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 import mongoose from "mongoose";
 import morgan from "morgan";
+import swaggerUI from "swagger-ui-express";
 mongoose.Promise = global.Promise;
 mongoose
   .connect("mongodb://localhost/invoice-builder")
@@ -8,11 +9,19 @@ mongoose
   .catch(err => console.log(err));
 const app = express();
 import { router } from "./config/routes";
+import swaggerDocument from "./config/swagger.json";
 const PORT = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(morgan("dev"));
+app.use(
+  "api-doc",
+  swaggerUI.serve,
+  swaggerUI.setup(swaggerDocument, {
+    explorer: true
+  })
+);
 app.use("/api", router);
 app.use((req, res, next) => {
   const error = new Error("Not Found");
